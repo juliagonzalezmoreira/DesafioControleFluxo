e8666161607912522f84caa6a36d5f54e5d0f7da
julia <juliagonzalezmoreira@gmail.com>
2023-09-15 14:15:49 -0300

    add contador java

---
+++ Contador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+        try{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametroUm =  scanner.nextInt();
+		System.out.println("Digite o segundo parâmetro:");
+		int parametroDois = scanner.nextInt();
+
+        contar(parametroUm, parametroDois);
+        scanner.close();
+
+        } catch (ParametrosInvalidosException exceção) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroDois < parametroUm){
+            throw new ParametrosInvalidosException();
+        }
+
+		int contagem = parametroDois - parametroUm;
+        for (int num = 0; num <= contagem; num++);
+		//realizar o for para imprimir os números com base na variável contagem
+        System.out.println("Imprimindo número: " + contagem);
+       
+	}
+}
